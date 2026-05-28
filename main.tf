bd76075494ee5c3e83e21ca3750be67dda3f36a8
allanali85 <114769780+allanali85@users.noreply.github.com>
2026-05-28 13:14:45 -0600

    Autoscalling

--- main.tf
+++ main.tf
@@ -32,19 +32,6 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.1"
@@ -95,3 +82,21 @@ resource "aws_lb_target_group_attachment" "blog" {
   target_id        = aws_instance.blog.id
   port             = 80
 }
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.2.1"
+
+  name = "blog"
+
+  security_groups = [modules.blog_sg.security_group_id]
+  instance_type   = "t3.nano"
+  image_id        = data.aws_ami.app_ami.id
+
+  traffic_source_attachments {
+    blog_alb {
+      traffic_source_identyfier = aws_lb_target_group.blog.arn
+    }
+  }
+}
+
