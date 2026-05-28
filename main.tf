69ace59d80cf5393a23a608aa2e2fa0698a9d1c0
allanali85 <114769780+allanali85@users.noreply.github.com>
2026-05-28 10:37:13 -0600

    Use module for segurity group

--- main.tf
+++ main.tf
@@ -31,15 +31,15 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "5.3.1"
   name = "blog"
 
   vpc_id = data.aws_vpc.default.id
   
-  ingress_rules = ["http-80-tcp","http-443-tcp"]
+  ingress_rules = [http-80-tcp,http-443-tcp]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
+  egress_rules = [all-all]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
